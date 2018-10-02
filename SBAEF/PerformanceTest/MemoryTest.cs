32abb35f6c125f512bc6084a27600f3dc1f8b300
unknown <soumya.moharana@cognizant.com>
2018-10-02 11:24:51 +0530

    PerformanceTest

---
+++ SBAEF/PerformanceTest/MemoryTest.cs
@@ -0,0 +1,39 @@
+﻿using NBench;
+using ProjectTrackerAPI.Controllers;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Hosting;
+
+namespace PerformanceTest
+{
+    public class MemoryTest : PerformanceTestSuite<MemoryTest>
+    {
+        private const int NumberOfAdds = 1000000;
+        private const int EntrySize = 24;
+        private const int MaxExpectedMemory = NumberOfAdds * EntrySize;
+
+        private UsersController _userController;
+
+        [PerfBenchmark(RunMode = RunMode.Iterations, TestMode = TestMode.Measurement)]
+        [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+        public void AddMemoryMeasurement()
+        {
+            _userController = new UsersController();
+            _userController.Request = new HttpRequestMessage();
+            var config = new HttpConfiguration();
+            _userController.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, config);
+            HttpResponseMessage apiResponse = _userController.GetUsers();
+        }
+
+        [PerfBenchmark(RunMode = RunMode.Iterations, TestMode = TestMode.Test, Description = "getUser with capacity, add memory test.")]
+        [MemoryAssertion(MemoryMetric.TotalBytesAllocated, MustBe.LessThan, MaxExpectedMemory)]
+        public void AddMemory_PassingTest()
+        {
+            _userController = new UsersController();
+            _userController.Request = new HttpRequestMessage();
+            var config = new HttpConfiguration();
+            _userController.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, config);
+            HttpResponseMessage apiResponse = _userController.GetUsers();
+        }
+    }
+}
---
+++ SBAEF/PerformanceTest/UserMockData.cs
@@ -0,0 +1,66 @@
+﻿using ProjectTrackerEF.Models;
+using System.Collections.Generic;
+
+namespace PerformanceTest
+{
+    public class UserMockData
+    {
+        public static List<User> GetUsersResponse()
+        {
+            var lstResponse = new List<User>();
+            lstResponse.Add(new User
+            {
+                Employee_ID=100,FirstName="Soumya",LastName="Moharana",IsMgr=false,User_ID=1
+            });
+            lstResponse.Add(new User
+            {
+                Employee_ID = 101,
+                FirstName = "Snigdha",
+                LastName = "Jena",
+                IsMgr = true,
+                User_ID = 2
+            });
+            lstResponse.Add(new User
+            {
+                Employee_ID = 102,
+                FirstName = "Mothun",
+                LastName = "Mohapatra",
+                IsMgr = false,
+                User_ID = 3
+            });
+            
+            return lstResponse;
+        }
+        public static User GetUserResponse()
+        {
+            return new User
+            {
+                Employee_ID = 100,
+                FirstName = "Soumya",
+                LastName = "Moharana",
+                IsMgr = false,
+                User_ID = 1
+            };
+        }
+        public static User userRequest()
+        {
+            return new User
+            {
+                Employee_ID = 100,
+                FirstName = "Soumya",
+                LastName = "Moharana",
+                IsMgr = false,
+                User_ID = 1
+            };
+        }
+        public static User newUserRequest()
+        {
+            return new User
+            {
+                Employee_ID = 100,
+                FirstName = "Soumya",
+                LastName = "Moharana"
+            };
+        }
+    }
+}
