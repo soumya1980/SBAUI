ced9d3f665df76cb8233118050f2e68472f028b4
unknown <soumya.moharana@cognizant.com>
2018-10-02 12:04:34 +0530

    PerfmTestingforUser

---
+++ SBAEF/PerformanceTest/UserGCTest.cs
@@ -0,0 +1,48 @@
+﻿using NBench;
+using ProjectTrackerAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Hosting;
+
+namespace PerformanceTest
+{
+    public class UserGCTest : PerformanceTestSuite<UserGCTest>
+    {
+        private UsersController _userController = new UsersController();
+
+        [PerfBenchmark(RunMode = RunMode.Iterations, TestMode = TestMode.Measurement)]
+        [GcMeasurement(GcMetric.TotalCollections, GcGeneration.AllGc)]
+        public void MeasureGarbageCollections()
+        {
+            RunTest();
+        }
+        [PerfBenchmark(RunMode = RunMode.Iterations, TestMode = TestMode.Test)]
+        [GcThroughputAssertion(GcMetric.TotalCollections, GcGeneration.Gen0, MustBe.LessThan, 300)]
+        [GcThroughputAssertion(GcMetric.TotalCollections, GcGeneration.Gen1, MustBe.LessThan, 150)]
+        [GcThroughputAssertion(GcMetric.TotalCollections, GcGeneration.Gen2, MustBe.LessThan, 20)]
+        [GcTotalAssertion(GcMetric.TotalCollections, GcGeneration.Gen2, MustBe.LessThan, 50)]
+        public void TestGarbageCollections()
+        {
+            RunTest();
+        }
+        private void RunTest()
+        {
+            for (var i = 0; i < 50; i++)
+            {
+                for (var j = 0; j < 100; j++)
+                {
+                    _userController.Request = new HttpRequestMessage();
+                    var config = new HttpConfiguration();
+                    _userController.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, config);
+                    HttpResponseMessage apiResponse = _userController.GetUsers();
+                }
+                _userController.Dispose();
+            }
+        }
+    }
+}
---
+++ SBAEF/PerformanceTest/UserThoughPutTest.cs
@@ -0,0 +1,54 @@
+﻿using NBench;
+using ProjectTrackerAPI.Controllers;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Hosting;
+
+namespace PerformanceTest
+{
+    public class UserThoughPutTest : PerformanceTestSuite<UserThoughPutTest>
+    {
+        private UsersController _userController = new UsersController();
+        private const string AddCounterName = "AddCounter";
+        private Counter addCounter;
+        private const int AcceptableMinAddThroughput = 20000000;
+
+        [PerfSetup]
+        public void Setup(BenchmarkContext context)
+        {
+            addCounter = context.GetCounter(AddCounterName);
+        }
+
+        [PerfBenchmark(RunMode = RunMode.Throughput, TestMode = TestMode.Test)]
+        [CounterThroughputAssertion(AddCounterName, MustBe.GreaterThan, AcceptableMinAddThroughput)]
+        public void AddThroughput_ThroughputMode(BenchmarkContext context)
+        {
+            _userController.Request = new HttpRequestMessage();
+            var config = new HttpConfiguration();
+            _userController.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, config);
+            HttpResponseMessage apiResponse = _userController.GetUsers();
+            addCounter.Increment();
+        }
+
+        [PerfBenchmark(RunMode = RunMode.Iterations, TestMode = TestMode.Test)]
+        [CounterThroughputAssertion(AddCounterName, MustBe.GreaterThan, 100)]
+        public void AddThroughput_IterationsMode(BenchmarkContext context)
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                _userController.Request = new HttpRequestMessage();
+                var config = new HttpConfiguration();
+                _userController.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, config);
+                HttpResponseMessage apiResponse = _userController.GetUsers();
+                addCounter.Increment();
+            }
+        }
+
+        [PerfCleanup]
+        public void Cleanup(BenchmarkContext context)
+        {
+            _userController.Dispose();
+        }
+    }
+}
